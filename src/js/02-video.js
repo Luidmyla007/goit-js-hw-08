397903d08cd7d0dbe283ffc8fbdbeb86f1a7d9fd
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-22 17:42:58 +0200

    task2 start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,11 @@
 import Player from '@vimeo/player';
- 
+import throttle from 'lodash.throttle'; 
+
+const iFrame = document.querySelector("iframe");
+
+const player = new Player(iFrame);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
