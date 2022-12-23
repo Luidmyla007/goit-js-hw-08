45a5b61d0894187fb8d4f96bd7c20d5942579a18
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-23 19:41:01 +0200

    task2 fifnfsh

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,8 +4,25 @@ import throttle from 'lodash.throttle';
 const iFrame = document.querySelector("iframe");
 
 const player = new Player(iFrame);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+player.on('timeupdate', throttle(function (currentTime) {
+   localStorage.setItem(LOCALSTORAGE_KEY, currentTime.seconds);   
+}, 1000));
 
-player.on('play', function() {
-    console.log('played the video!');
+let seconds = Number(localStorage.getItem(LOCALSTORAGE_KEY));
+
+
+player.setCurrentTime(seconds).then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
 });
 
