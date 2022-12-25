2b21ee2d04a2e19db7c73339c218db3b10f1bd89
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-25 10:58:36 +0200

    task3 fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,40 +6,34 @@ const refs = {
     textarea: document.querySelector('.feedback-form textarea'),
 };
 const LOCALSTORAGE_KEY = "feedback-form-state";
-const SummaryData = {};
+let summaryData = {};
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.input.addEventListener('input', throttle(onEmailInput, 500));
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-refs.form.addEventListener('input', evt => {
-    SummaryData[evt.target.name] = evt.target.value;
-    console.log(SummaryData);
-})
+refs.form.addEventListener('input', throttle(onInput, 500));
 
+pageUpdate();
 
-function onFormSubmit(evt) {
-    evt.preventDefault();
-    evt.currentTarget.reset();
+function onInput(event) {
+    summaryData[event.target.name] = event.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(summaryData));
+}
+
+function pageUpdate() {
+    const savedText = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)); 
+    if (savedText) {
+       for (const [key, value] of Object.entries(savedText)) {
+           refs.form[key].value = value;
+           summaryData[key] = value;
+        }
+   }
+      
+ }
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    console.dir(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
     localStorage.removeItem(LOCALSTORAGE_KEY);
-}
-
-function onEmailInput(evt) {
-    const userEmail = evt.target.value; 
-    localStorage.setItem(LOCALSTORAGE_KEY, userEmail);
-     const saveEmail = localStorage.getItem(LOCALSTORAGE_KEY);
-    if (saveEmail) {
-        refs.input.value = saveEmail;
-    }
-}
-
-
-function onTextareaInput(evt) {
-    const message = evt.target.value; 
-    localStorage.setItem(LOCALSTORAGE_KEY, message);
-    const savedMessage = localStorage.getItem (LOCALSTORAGE_KEY);
-    if (savedMessage) {
-        refs.textarea.value = savedMessage;
-    }
 }
 
 
